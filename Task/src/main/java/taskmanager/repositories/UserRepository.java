bed733ba017bb60a0b5f799656f133898710adee
mathfrancisco2 <math.francisco2@gmail.com>
2024-09-23 13:13:41 -0300

    att
    adicionado userservices

--- Task/src/main/java/taskmanager/repositories/UserRepository.java
+++ Task/src/main/java/taskmanager/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package taskmanager.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 import taskmanager.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    UserDetails findFirstByEmail(String username);
 }
