c758902f2e01e2ceaa941bd638c9a3498908cc67
mathfrancisco2 <math.francisco2@gmail.com>
2024-09-28 12:52:05 -0300

    att

--- taskfront/task_angular/src/app/auth/components/signup/signup.component.ts
+++ taskfront/task_angular/src/app/auth/components/signup/signup.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormBuilder,FormGroup, Validators } from "@angular/forms";
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent {
+  signupForm!: FormGroup;
+  hidePassword=true;
 
+  constructor(private fb: FormBuilder){
+    this.signupForm = this.fb.group({
+      name: [null,[Validators.required]],
+      email: [null,[Validators.required,Validators.email]],
+      password: [null,[Validators.required]],
+      confirmPassword: [null,[Validators.required]],
+    })
+  }
+  togglePasswordVisibility(){
+   this.hidePassword=!this.hidePassword
+  }
 }
