21420994b04fc62bf4f54baa2d37fa9f0af724d7
mathfrancisco2 <math.francisco2@gmail.com>
2024-09-29 22:55:55 -0300

    att

--- taskfront/task_angular/src/app/auth/components/login/login.component.ts
+++ taskfront/task_angular/src/app/auth/components/login/login.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  hidePassword = true;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  initForm() {
+    this.loginForm = this.fb.group({
+      email: [null, [Validators.required, Validators.email]],
+      password: [null, [Validators.required]]
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+      // Implement your login logic here
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 }
