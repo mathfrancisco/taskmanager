79937ccfcb058d0965f79c3e44d7193db92e3230
mathfrancisco2 <math.francisco2@gmail.com>
2024-10-09 20:51:42 -0300

    att

--- taskfront/task_angular/src/app/modules/employee/components/dashboard/dashboard.component.ts
+++ taskfront/task_angular/src/app/modules/employee/components/dashboard/dashboard.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from "../../services/employee.service";
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  listOfTasks: any = [];
 
+  constructor(private service: EmployeeService) {}
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.service.getEmployeeTasksById().subscribe((res) => {
+      console.log(res);
+      this.listOfTasks = res;
+    });
+  }
 }
--- taskfront/task_angular/src/app/modules/employee/services/employee.service.ts
+++ taskfront/task_angular/src/app/modules/employee/services/employee.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {StorageService} from "../../../auth/services/storage/storage.service";
+import {Observable} from "rxjs";
 
+const BASIC_URL = "http://localhost:8080/";
 @Injectable({
   providedIn: 'root'
 })
+
 export class EmployeeService {
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+
+}
+  getEmployeeTasksById(): Observable<any> {
+    return this.http.get(BASIC_URL + "api/employee/tasks", {
+      headers: this.createAuthorizationHeader()
+    })
+  }
+
+  private createAuthorizationHeader(): HttpHeaders {
+    return new HttpHeaders().set('Authorization', 'Bearer ' + StorageService.getToken())
+  }
 }
