7edeaa29ab5ee9113bff5bae35ba55f04ab90dc9
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-10-15 08:56:07 -0300

    Update user_data.sh

--- infra/user_data.sh
+++ infra/user_data.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
+# Switch to root user
+sudo su
+
+# Update the system and install Docker
+yum update -y
+yum install -y docker
+
+# Start Docker service
+service docker start
+
+# Add ec2-user to the docker group
+usermod -a -G docker ec2-user
+
 # Install Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Create directory for Docker Compose
 mkdir -p /var/app/current
