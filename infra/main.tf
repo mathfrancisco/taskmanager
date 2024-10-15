35d410e838e679b7964867979e71024865cd844d
Matheus Francisco <81334745+mathfrancisco@users.noreply.github.com>
2024-10-15 08:03:59 -0300

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,14 +10,12 @@ resource "aws_elastic_beanstalk_application" "app" {
 resource "aws_security_group" "securitygroup" {
   name        = "securitygroup"
   description = "Permitir acesso HTTP e acesso a Internet"
-
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
   egress {
     from_port   = 0
     to_port     = 65535
@@ -33,32 +31,8 @@ resource "aws_elastic_beanstalk_environment" "env" {
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
-    name      = "DisableIMDSv1"
-    value     = "true"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "InstanceType"
-    value     = "t2.nano"
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "SecurityGroups"
-    value     = aws_security_group.securitygroup.id
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
     name      = "IamInstanceProfile"
     value     = aws_iam_instance_profile.eb_profile.name
-  }
-
-  setting {
-    namespace = "aws:autoscaling:launchconfiguration"
-    name      = "EC2KeyName"
-    value     = "chaveum"
   }
 
   setting {
@@ -79,29 +53,23 @@ resource "aws_elastic_beanstalk_environment" "env" {
     value     = "subnet-053d508166aa85895,subnet-05c26f9ac7c48daf6"
   }
 
-  # Configurações da aplicação (variáveis de ambiente)
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "MYSQL_ROOT_PASSWORD"
-    value     = "root"
+    name      = "COMPOSE_PROJECT_NAME"
+    value     = "taskmanager"
   }
 
   setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "MYSQL_DATABASE"
-    value     = "task_db"
+    namespace = "aws:elasticbeanstalk:environment:proxy"
+    name      = "ProxyServer"
+    value     = "nginx"
   }
 
+  # Use o script de usuário para configurar o Docker Compose
   setting {
     namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "SPRING_PROFILES_ACTIVE"
-    value     = "dev"
-  }
-
-  setting {
-    namespace = "aws:elasticbeanstalk:application:environment"
-    name      = "SPRING_DATASOURCE_URL"
-    value     = "jdbc:mysql://mysql:3306/task_db"
+    name      = "USER_DATA_SCRIPT"
+    value     = base64encode(file("user_data.sh"))
   }
 }
 
@@ -112,24 +80,23 @@ resource "aws_iam_instance_profile" "eb_profile" {
 
 resource "aws_iam_role" "eb_role" {
   name               = "eb-ec2-role"
-
   assume_role_policy = jsonencode({
-    Version   :   "2012-10-17",
-    Statement : [
+    Version = "2012-10-17",
+    Statement = [
       {
-        Action   : "sts:AssumeRole",
-        Effect   : "Allow",
-        Principal : { Service : "ec2.amazonaws.com" }
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = { Service = "ec2.amazonaws.com" }
       }
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "eb_web_tier" {
-  policy_arn= "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-  role= aws_iam_role.eb_role.id
+  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role       = aws_iam_role.eb_role.id
 }
 
 output "eb_url" {
-  value= aws_elastic_beanstalk_environment.env.endpoint_url
+  value = aws_elastic_beanstalk_environment.env.endpoint_url
 }
