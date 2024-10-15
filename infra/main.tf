0ca2d82627a647891fa8e19c8a9f821606053a25
mathfrancisco2 <math.francisco2@gmail.com>
2024-10-14 23:35:07 -0300

    att

--- infra/main.tf
+++ infra/main.tf
@@ -29,7 +29,13 @@ resource "aws_security_group" "securitygroup" {
 resource "aws_elastic_beanstalk_environment" "env" {
   name                = "taskmanager-env"
   application         = aws_elastic_beanstalk_application.app.name
-  solution_stack_name = "64bit Amazon Linux 2 v3.5.7 running Multi-container Docker"
+  solution_stack_name = "64bit Amazon Linux 2023 v4.3.8 running Docker"
+
+  setting {
+    namespace = "aws:autoscaling:launchconfiguration"
+    name      = "DisableIMDSv1"
+    value     = "true"
+  }
 
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
@@ -52,13 +58,25 @@ resource "aws_elastic_beanstalk_environment" "env" {
   setting {
     namespace = "aws:autoscaling:launchconfiguration"
     name      = "EC2KeyName"
-    value     = "chaveum"  # Coloque o nome do seu par de chaves EC2
+    value     = "chaveum"
   }
 
   setting {
     namespace = "aws:elasticbeanstalk:environment"
     name      = "EnvironmentType"
     value     = "SingleInstance"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "VPCId"
+    value     = "vpc-07632126143031ff9"
+  }
+
+  setting {
+    namespace = "aws:ec2:vpc"
+    name      = "Subnets"
+    value     = "subnet-053d508166aa85895,subnet-05c26f9ac7c48daf6"
   }
 
   # Configurações da aplicação (variáveis de ambiente)
@@ -93,25 +111,25 @@ resource "aws_iam_instance_profile" "eb_profile" {
 }
 
 resource "aws_iam_role" "eb_role" {
-  name = "eb-ec2-role"
+  name               = "eb-ec2-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
+    Version   :   "2012-10-17",
+    Statement : [
       {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = { Service = "ec2.amazonaws.com" }
+        Action   : "sts:AssumeRole",
+        Effect   : "Allow",
+        Principal : { Service : "ec2.amazonaws.com" }
       }
     ]
   })
 }
 
 resource "aws_iam_role_policy_attachment" "eb_web_tier" {
-  policy_arn = "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
-  role       = aws_iam_role.eb_role.id
+  policy_arn= "arn:aws:iam::aws:policy/AWSElasticBeanstalkWebTier"
+  role= aws_iam_role.eb_role.id
 }
 
 output "eb_url" {
-  value = aws_elastic_beanstalk_environment.env.endpoint_url
+  value= aws_elastic_beanstalk_environment.env.endpoint_url
 }
--- infra/user_data.sh
+++ infra/user_data.sh
@@ -2,7 +2,7 @@
 # Atualiza o sistema e instala o Docker
 sudo su
 yum update -y
-amazon-linux-extras install docker
+amazon-linux-extras install docker -y
 
 # Inicia o serviço Docker e habilita no boot
 service docker start
@@ -10,7 +10,7 @@ usermod -a -G docker ec2-user
 systemctl enable docker
 
 # Instala o Docker Compose
-curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "<https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)>" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
 # Cria diretório para o Docker Compose
@@ -20,45 +20,39 @@ mkdir -p /var/app/current
 cat <<EOT >> /var/app/current/docker-compose.yml
 version: '3'
 services:
-  mysql:
-    image: mysql:8.0
-    container_name: mysql
-    environment:
-      MYSQL_ROOT_PASSWORD: root
-      MYSQL_DATABASE: task_db
-    ports:
-      - "3306:3306"
-    volumes:
-      - ./mysql-data:/var/lib/mysql
-    networks:
-      - app-network
+ mysql:
+ image: mysql:8.0
+ container_name: mysql
+ environment:
+ MYSQL_ROOT_PASSWORD: root
+ MYSQL_DATABASE: task_db
+ ports:
+ - '3306:3306'
+ volumes:
+ - ./mysql-data:/var/lib/mysql
 
-  backend:
-    container_name: task-back
-    image: mathfrancisco/todolist-backend:latest
-    environment:
-      SPRING_PROFILES_ACTIVE: dev
-      SPRING_DATASOURCE_URL: jdbc:mysql://mysql:3306/task_db
-    ports:
-      - "8080:8080"
-    depends_on:
-      - mysql
-    networks:
-      - app-network
+ backend:
+ container_name: task-back
+ image: mathfrancisco/todolist-backend:latest
+ environment:
+ SPRING_PROFILES_ACTIVE: dev
+ SPRING_DATASOURCE_URL: jdbc:mysql://mysql:3306/task_db
+ ports:
+ - '8080:8080'
+ depends_on:
+ - mysql
 
-  frontend:
-    container_name: task-front
-    image: mathfrancisco/todolist-frontend:latest
-    ports:
-      - "80:80"
-    depends_on:
-      - backend
-    networks:
-      - app-network
+ frontend:
+ container_name: task-front
+ image: mathfrancisco/todolist-frontend:latest
+ ports:
+ - '80:80'
+ depends_on:
+ - backend
 
 networks:
-  app-network:
-    driver: bridge
+ app-network:
+ driver: bridge
 EOT
 
 # Rodar o Docker Compose
